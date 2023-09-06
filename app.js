2065aba5c132a54c9f094c2261447681f440fd42
Vadim Bykov <miguellorenz@yandex.ru>
2023-09-06 12:16:39 +0300

    Tests 14

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const errorHandler = require('./middlewares/error-handler');
-// console.log(require('http2').constants);
+
+const indexRoutes = require('./routes/index');
+
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
-
-app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -22,22 +18,29 @@ app.use(limiter);
 
 app.use(helmet());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.use(requestLogger);
-
-app.use('/', require('./routes/index'));
-
-app.use(errorLogger);
+app.use('/', indexRoutes);
 
 app.use(errors());
 
-app.use(errorHandler);
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
 
 app.listen(PORT);
